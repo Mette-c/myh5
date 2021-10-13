a7955c9ce8a2a140c0e4741a0626dcf3402eaf4d
cencen <email@example.com>
2021-10-13 13:51:34 +0800

    文件第一次添加

---
+++ src/utils/tools.js
@@ -0,0 +1,15 @@
+//ajax请求封装
+function request(url,callback,method='get') {
+    //设置基础路径
+    const baseUrl = "http://120.76.247.5:2003/api";
+    url = baseUrl+url;
+    //创建一个异步请求
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        let data = JSON.parse(xhr.responseText);
+        callback(data);
+    }
+    //设置请求参数
+    xhr.open(method,url,true);
+    xhr.send();
+}
